82e729c3327cc8d0f9fd7d212de9270533d3245c
Павел Шаповал <40662945+PavelShapoval@users.noreply.github.com>
2018-10-26 14:32:25 +0600

    Update addscript.js

--- addscript.js
+++ addscript.js
@@ -17,3 +17,29 @@ $(function() {
     }
 
 });                       
+/*проверки по урл на js*/
+
+		/*add>*/
+		// jQuery(function(){
+	 //    	var test = window.location.href.length;
+		
+	 //    	console.log(test);
+	 //    	if (test > 20){
+	 //    		jQuery('.slider-head-text').text('123');
+	 //    	}
+	 //    });
+
+	   	jQuery(function(){
+	    	var test = window.location.href.length;
+	    	//console.log(test);
+	    	if (test <= 20){
+			    jQuery(window).scroll(function(){
+			        if(jQuery(this).scrollTop()>700){
+			            jQuery('.header-phone p a').css('color', '#8ac535');
+			        }
+			        else if (jQuery(this).scrollTop()<700){
+			            jQuery('.header-phone p a').css('color','#fff');
+			        }
+			    });
+			}
+	    });
